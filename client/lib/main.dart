89dc83d8d4061e9a7cf845c24c0c472e3573379b
simong2 <simon.getahun@icloud.com>
2025-02-06 19:42:08 -0500

    app title

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'MezmurSearch',
+      title: 'YT Search',
       // theme: ThemeData(
       //
       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
